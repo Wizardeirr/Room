8ed3628253c7ba2dac8144463aaea99e7df10d63
volkan kelleci <kellecivolkan@gmail.com>
2023-06-16 16:14:28 +0300

    Glide added Module,

--- app/src/main/java/com/example/room/di/Module.kt
+++ app/src/main/java/com/example/room/di/Module.kt
@@ -2,7 +2,13 @@ package com.example.room.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.room.R
+import com.example.room.repo.ModelRepositoryIF
+import com.example.room.repo.Repository
 import com.example.room.retrofit.ModelAPIService
+import com.example.room.room.ModelDao
 import com.example.room.room.ModelDataBase
 import com.example.room.util.utility.BASE_URL
 import dagger.Module
@@ -35,5 +41,16 @@ object Module {
         addConverterFactory(GsonConverterFactory.create()).
                 baseUrl(BASE_URL).build().create()
     }
+    @Singleton
+    @Provides
+    fun injectNormalRepo(dao:ModelDao,api:ModelAPIService)=Repository(dao,api) as ModelRepositoryIF
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context)= Glide.with(context)
+        .setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+        )
     
 }
