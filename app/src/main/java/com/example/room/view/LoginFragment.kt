03a32e44d6aca28a8a2a43bdd994bcf1fbb1c60a
volkan kelleci <kellecivolkan@gmail.com>
2023-06-16 16:54:39 +0300

    Login fragment done

--- app/src/main/java/com/example/room/view/LoginFragment.kt
+++ app/src/main/java/com/example/room/view/LoginFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.room.adapter.ArtRecyclerAdapter
 import com.example.room.databinding.FragmentLoginBinding
 import com.example.room.util.BaseVBFragment
@@ -18,6 +20,23 @@ class LoginFragment @Inject constructor(
     private val artRecyclerAdapter: ArtRecyclerAdapter
 ) : BaseVBFragment<FragmentLoginBinding>() {
     private lateinit var viewModel: ModelViewModel
+
+    private val swipeCallback=object : ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder,
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val layoutPosition=viewHolder.layoutPosition
+            val deleteModel=artRecyclerAdapter.models[layoutPosition]
+            viewModel.deleteModel(deleteModel)
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
